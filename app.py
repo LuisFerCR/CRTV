01214cfd8f93977a5c25f807015e0a29556bf024
LuisFerCR <49252529+LuisFerCR@users.noreply.github.com>
2019-05-11 20:04:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ async def ws_handler(websocket, path):
         print("")
 
 
-port = int(os.getenv('PORT', 80))#5687
+port = int(os.getenv('PORT', 8080))#5687
 start_server = websockets.serve(ws_handler, '0.0.0.0', port, klass=HttpWSSProtocol)
 #logger.info('Listening on port %d', port)
 
