d24916bd1ce996ac98e97701efc1b6d967ef93e9
LuisFerCR <49252529+LuisFerCR@users.noreply.github.com>
2019-05-11 18:36:04 -0600

    Add files via upload

--- ESP8266/ESP8266_WebSocketClient/ESP8266_WebSocketClient.ino
+++ ESP8266/ESP8266_WebSocketClient/ESP8266_WebSocketClient.ino
@@ -1,128 +1,106 @@
 /*
- *Netmedias
  *
- *  Created on: 20.08.2015
+ *IoT Technologies
+ *
+ *  Created on: 02-05-2019
+ *  Basado en los siguientes proyectos:
  *  
  */
+
+// Inclusión de librerías
+#include <Arduino.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include <WebSocketsClient.h>
 #include <Hash.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <DNSServer.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <Ticker.h>
 
-// @@@@@@@@@@@@@@@ You only need to midify modify wi-fi and domain info @@@@@@@@@@@@@@@@@@@@
-const char* ssid     = "enter your ssid"; //enter your ssid/ wi-fi(case sensitiv) router name - 2.4 Ghz only
-const char* password = "enter ssid password";     // enter ssid password (case sensitiv)
-char host[] = "alexaskillsiot.herokuapp.com"; //enter your Heroku domain name like "espiot.herokuapp.com" 
-int sensor_distance_from_door = 5; // enter the distance in inches between you ultrasonic sensor and garage door
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//Firstly the connections of ultrasonic Sensor.Connect 3.9v to +5v and GND. Trigger pin to 5 & echo pin to 4. 
-#define trigPin 5
-#define echoPin 4
-int duration, distance;
-const int relayPin1 = 16; //Garage door 1
+
+//Variables del entorno
+Ticker ticker;
+char host[] = "tvcloudservice.herokuapp.com"; //Nombre de la App en Heroku. Ejemplo:"miaplicación.herokuapp.com" 
+MDNSResponder mdns;
+ESP8266WebServer server(80);
+IRsend irsend(4);  // Puerto GPIO pin 4 (D2) del ESP8266 que controla al LED Infrarrojo.
+uint16_t rawData[41] = {2672, 882,  448, 892,  446, 444,  446, 446,  448, 874,  892, 450,  448, 444,  446, 446,  448, 442,  446, 446,  446, 444,  448, 444,  446, 446,  446, 444,  448, 444,  446, 444,  892, 892,  478, 414,  444, 448,  448, 444,  446};
 int pingCount = 0;
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
+String niveles;
 int port = 80;
 char path[] = "/ws"; 
 ESP8266WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
-
-DynamicJsonBuffer jsonBuffer;
+const size_t capacity = 6*JSON_ARRAY_SIZE(1) + 2*JSON_OBJECT_SIZE(0) + 10*JSON_OBJECT_SIZE(1) + 6*JSON_OBJECT_SIZE(2) + 4*JSON_OBJECT_SIZE(3) + 3*JSON_OBJECT_SIZE(4) + 4*JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 3760;
+DynamicJsonDocument doc(capacity);
 String currState, oldState, message;
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) { 
 
-
-    switch(type) {
-        case WStype_DISCONNECTED:
-            //USE_SERIAL.printf("[WSc] Disconnected!\n");
-           Serial.println("Disconnected! ");
-            break;
-            
-        case WStype_CONNECTED:
-            {
-             Serial.println("Connected! ");
-			    // send message to server when Connected
-				    webSocket.sendTXT("Connected");
-            }
-            break;
-            
-        case WStype_TEXT:
-            Serial.println("Got data");
-            //Serial.println("looping...");
-            digitalWrite(trigPin, HIGH);
-            delayMicroseconds(10);
-            digitalWrite(trigPin, LOW);
-            duration = pulseIn(echoPin, HIGH);
-            distance = (duration/2) / 74; //Inches
-            distance = (duration/2) / 29.1; //centimeter
-            
-            if (distance <= sensor_distance_from_door ){
-              //Serial.print(distance);
-              currState = "open";
-              //Serial.println(currState);
-            }else{
-              //Serial.println(currState);
-              currState = "close";
-            }
-            
-            processWebScoketRequest((char*)payload);
-
-            break;
-            
-        case WStype_BIN:
-
-            hexdump(payload, length);
-            Serial.print("Got bin");
-            // send data to server
-            // webSocket.sendBIN(payload, length);
-            break;
-    }
-
-}
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
+  //Inicializa función IR.
+  irsend.begin(); 
+  Serial.setDebugOutput(true);
+  //set led pin as output
+  pinMode(BUILTIN_LED, OUTPUT);
+  // inicializa parpadeo de indicador LED cada 0,5 segundos dado ingreso a modo AP del ESP8266.
+  ticker.attach(0.6, tick);
+  //WiFiManager
+  //Inicialización de variable. Solo se requiere realizar una vez.
+  WiFiManager wifiManager;
+  //Si se desea evitar que el módulo WiFi utilice la información preconfigurada de SSID y password, se puede habilitar la siguiente línea.
+  wifiManager.resetSettings();
 
-    pinMode(trigPin, OUTPUT); 
-    pinMode(echoPin, INPUT);
+  // Esta función se activa cuando la conexión a WiFi con la conexión falla utilizando el SSID y password almacenado anteriormente. Permite ingresar a modo de AP.
+  wifiManager.setAPCallback(configModeCallback);
+
+  //Almacena el SSID y password. Inicia conexión al WiFi.
+  if (!wifiManager.autoConnect()) {
+    Serial.println("Conexión fallida");
+    //reinicia ESP para intentar nuevamente la conexión.
+    ESP.reset();
+    delay(1000);
+  }
+
+  //en este punto la conexión WiFi está establecida. Se desactiva parpadeo del LED y se mantiene permanentemente en modo encendido.
+  ticker.detach();
+  digitalWrite(BUILTIN_LED, LOW);
 
     
-    Serial.setDebugOutput(true);
-    
-    pinMode(relayPin1, OUTPUT);
+  Serial.println("Conectado a la red WiFi");
+  Serial.println("Dirección IP: "+WiFi.localIP());
+  //Inicializa websocket.
+  webSocket.begin(host, port, path);
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000);
 
-    
-      for(uint8_t t = 4; t > 0; t--) {
-          delay(1000);
-      }
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    
-    //Serial.println(ssid);
-    WiFiMulti.addAP(ssid, password);
+  if (mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS responder inicializado");
+  }
+  //Activa servicios del servidor.
+  server.on("/", handleRoot);
+  server.on("/ir", handleIr);
+  server.on("/inline", [](){
+  server.send(200, "text/plain", "Servidor activado");
+  });
 
-    //WiFi.disconnect();
-    while(WiFiMulti.run() != WL_CONNECTED) {
-      Serial.print(".");
-        delay(1000);
-    }
-    Serial.println("Connected to wi-fi");
-    webSocket.begin(host, port, path);
-    webSocket.onEvent(webSocketEvent);
-
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("Servidor HTTP inicializado");
 }
 
 void loop() {
-
-    webSocket.loop();
-        delay(100);
-	// make sure after every 40 seconds send a ping to Heroku
-	//so it does not terminate the websocket connection
-	//This is to keep the conncetion alive between ESP and Heroku
-	if (pingCount > 20) {
+  server.handleClient();
+  webSocket.loop();
+  //Se establece un ciclo de 20s para mantener el keepalive activo entre Heroku y el EPS8266.
+  delay(2000);
+	if (pingCount > 10) {
 		pingCount = 0;
 		webSocket.sendTXT("\"heartbeat\":\"keepalive\"");
 	}
@@ -131,61 +109,204 @@ void loop() {
 	}
 }
 
+//Módulo para inversión de estado encendido/apagado del BUILTIN_LED. Se utiliza para indicar el estado de conexión a WiFi.
+void tick()
+{
+  int state = digitalRead(BUILTIN_LED);  // Leer el estado actual del LED interno
+  digitalWrite(BUILTIN_LED, !state);     // Invertir el estado actual del LED interno
+}
 
-void processWebScoketRequest(String data){
-            String jsonResponse = "{\"version\": \"1.0\",\"sessionAttributes\": {},\"response\": {\"outputSpeech\": {\"type\": \"PlainText\",\"text\": \"<text>\"},\"shouldEndSession\": true}}";
-            JsonObject& req = jsonBuffer.parseObject(data);
+// Inicializa estado de ingreso al modo de configuración WiFi
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  //Al ingresar al modo de configuración WiFi, el indicador LED parpadea más rápidamente.
+  ticker.attach(0.2, tick);
+}
 
-            String instance = req["instance"];
-            String state = req["state"];
-            String query = req["query"];
-            String message = "{\"event\": \"OK\"}";
-            
-            Serial.println("Data2-->"+data);
-            Serial.println("State-->" + state);
+//configuración base de página web.
+void handleRoot() {
+  server.send(200, "text/html",
+              "<html>" \
+                "<head><title>CRTV</title></head>" \
+                "<body>" \
+                  "<h1>Hola! Bienvenido a CRTV. </h1>Selecciona la opcion que deseas ejecutar. CRTV hara el resto por ti!</h1>" \
+                  "<p><a href=\"ir?code=12\">Encendido/Apagado</a></p>" \
+                  "<p><a href=\"ir?code=16\">Subir volumen</a></p>" \
+                  "<p><a href=\"ir?code=17\">Bajar volumen</a></p>" \
+                  "<p><a href=\"ir?code=32\">Subir canal</a></p>" \
+                  "<p><a href=\"ir?code=33\">Bajar canal</a></p>" \
+                  "<p><a href=\"ir?code=13\">Mute</a></p>" \        
+                "</body>" \
+              "</html>");
+}
 
-            if(query == "?"){ //if command then execute
-              Serial.println("Recieved query!");
-                 if(currState=="open"){
-                      message = "open";
-                    }else{
-                      message = "closed";
-                    }
-                   jsonResponse.replace("<text>", "Garage door " + instance + " is " + message );
-                   webSocket.sendTXT(jsonResponse);
-                   
-            }else if(query == "cmd"){ //if query check state
-              Serial.println("Recieved command!");
-                   if(state != currState){
-                         if(currState == "close"){
-                            message = "opening";
-                          }else{
-                            message = "closing";
-                          }
-                          digitalWrite(relayPin1, HIGH);
-                          delay(1000);
-                          digitalWrite(relayPin1, LOW);
-                   }else{
-                          if(currState == "close"){
-                            message = "already closed";
-                          }else{
-                            message = "already open";
-                          }
-                    }
-                  jsonResponse.replace("<text>", "Garage door " + instance + " is " + message );
-                  webSocket.sendTXT(jsonResponse);
+//Módulo de gestión de IR.
+void handleIr() {
+  for (uint8_t i = 0; i < server.args(); i++) {
+    if (server.argName(i) == "code") {
+      uint32_t code = strtoul(server.arg(i).c_str(), NULL, 10);
+      irsend.sendRC6(code, 20);
+      Serial.println(code);
+      delay(100);
+    }
+  }
+  handleRoot();
+}
 
-            
-            }else{//can not recognized the command
-                    Serial.println("Command is not recognized!");
-                   jsonResponse.replace("<text>", "Command is not recognized by garage door Alexa skill");
-                   webSocket.sendTXT(jsonResponse);
-            }
-            Serial.print("Sending response back");
-            Serial.println(jsonResponse);
-                  // send message to server
-                  webSocket.sendTXT(jsonResponse);
+//Módulo de manejo de fallo.
+void handleNotFound() {
+  String message = "Archivo no encontrado\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++)
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  server.send(404, "text/plain", message);
 }
 
 
+//Gestión de eventos del Websocket
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) { 
 
+    switch(type) {
+        case WStype_DISCONNECTED:
+            Serial.println("Desconectado!");
+            Serial.println("Conectando...");
+            webSocket.begin(host, port, path);          
+            webSocket.onEvent(webSocketEvent);
+            break;
+            
+        case WStype_CONNECTED:
+            Serial.println("Conectado! ");
+            //Enviar mensaje al servidor cuando se ha establecido conexión.
+            webSocket.sendTXT("Connected");
+            break;
+            
+        case WStype_TEXT:
+            Serial.println("Datos recibidos");
+            //Serial.println((char*)payload);
+            Serial.println("Analizando...");
+            //Analiza datos recibidos.
+            processWebScoketRequest((char*)payload);
+            break;
+            
+        case WStype_BIN:
+            hexdump(payload, length);
+            Serial.print("Datos binarios recibidos");
+            // Enviar datos al servidor.
+            //webSocket.sendBIN(payload, length);
+            break;
+    }
+}
+
+//Módulo de procesamiento de datos obtenidos a través del websocket
+void processWebScoketRequest(char* data)
+{
+            String jsonResponse = "{\"version\": \"1.0\",\"sessionAttributes\": {},\"response\": {\"outputSpeech\": {\"type\": \"PlainText\",\"text\": \"<text>\"},\"shouldEndSession\": true}}";
+            //Serial.println(data);
+            //Arduino JSON Tool: https://arduinojson.org/v5/assistant/
+            deserializeJson(doc, data, DeserializationOption::NestingLimit(11));
+            JsonObject request = doc["request"];
+            JsonObject request_intent = request["intent"];
+            JsonObject request_intent_slots = request_intent["slots"];
+            JsonObject request_intent_slots_instance = request_intent_slots["instancia"];
+            JsonObject request_intent_slots_state = request_intent_slots["estado"];
+
+            String instancia = request_intent_slots_instance["value"];
+            String estate = request_intent_slots_state["value"];
+            String pregunta = request_intent_slots["pregunta"]["confirmationStatus"];
+            
+            Serial.println(instancia);
+            Serial.println(estate);
+            Serial.println(pregunta);
+            
+            if(pregunta == "?"){ //if command then execute
+              Serial.println("Consulta de estado recibida!");
+             jsonResponse.replace("<text>", "Consulta de estado recibida");
+             webSocket.sendTXT(jsonResponse);
+                   
+            }else if(instancia == "volumen"){ 
+              if((estate == "suba") || (estate == "sube") || (estate == "incremente") || (estate == "incrementa")){
+                  for (uint8_t i = 0; i < 5; i++){
+                    irsend.sendRC6(16, 20);
+                    delay(500);
+                  }
+                  Serial.println("Subir Volumen");
+              }else if((estate == "baje") || (estate == "baja") || (estate == "disminuye") || (estate == "disminuya")){
+                  for (uint8_t i = 0; i < 5; i++){
+                    irsend.sendRC6(17, 20);
+                    delay(500);
+                  }
+                  Serial.println("Bajar Volumen"); 
+              }else if((estate == "suba 2") || (estate == "suba 3") || (estate == "suba 4")|| (estate == "suba 5")|| (estate == "suba 6")|| (estate == "suba 7")){
+                  niveles = estate[5];
+                  for (uint8_t i = 0; i < niveles.toInt(); i++){ //El caracter [5] del string estate corresponde al número de niveles.
+                    irsend.sendRC6(16, 20);
+                    delay(500);
+                  }
+                  Serial.println("Subir x niveles el volumen"); 
+              }
+              else if((estate == "baja 2") || (estate == "baja 3") || (estate == "baja 4")|| (estate == "baja 5")|| (estate == "baja 6")|| (estate == "baja 7")){
+                  niveles = estate[5];
+                  for (uint8_t i = 0; i < niveles.toInt(); i++){ //El caracter [5] del string estate corresponde al número de niveles.
+                    irsend.sendRC6(17, 20);
+                    delay(500);
+                  }
+                  Serial.println("Bajar x niveles el volumen"); 
+              }        
+              jsonResponse.replace("<text>", "Ok");
+              webSocket.sendTXT(jsonResponse);    
+            }else if(instancia == "silencio"){ 
+              if((estate == "active")|| (estate == "activa") ){
+                  irsend.sendRC6(13, 20);
+                  Serial.println("Modo silencio activado");
+              }else if((estate == "desactive") || (estate == "desactiva") ){
+                  irsend.sendRC6(13, 20);
+                  Serial.println("Modo silencio desactivado");
+              }      
+              jsonResponse.replace("<text>", "Ok");
+              webSocket.sendTXT(jsonResponse);    
+            }else if(instancia == "control"){ 
+              if((estate == "activa") || (estate == "active") || (estate == "encienda")  || (estate == "enciende") ){
+                  irsend.sendRC6(12, 20);
+                  Serial.println("TV encendido");
+              }else if((estate == "desactive")  || (estate == "apaga") || (estate == "apague")){
+                  irsend.sendRC6(12, 20);
+                  Serial.println("TV apagado");
+              }    
+              jsonResponse.replace("<text>", "Ok");
+              webSocket.sendTXT(jsonResponse);    
+            }else if(instancia == "canal"){ 
+              if(estate == "siguiente"){
+                  irsend.sendRC6(32, 20);
+                  Serial.println("Subir canal");
+              }else if(estate == "anterior"){
+                  irsend.sendRC6(33, 20);
+                  Serial.println("Bajar canal");
+              }else if(estate == "7"){
+                  irsend.sendRC6(7, 20);
+                  Serial.println("Canal 7");
+              }else if(estate == "45"){
+                  irsend.sendRC6(4, 20);
+                  delay(200);
+                  irsend.sendRC6(5, 20);
+                  Serial.println("Canal 45");
+              }       
+              jsonResponse.replace("<text>", "Ok");
+              webSocket.sendTXT(jsonResponse);                
+            }else{//en caso de que no se reconozca el comando.
+                   Serial.println("Comando no reconocido!");
+                   jsonResponse.replace("<text>", "repita el comando");
+                   webSocket.sendTXT(jsonResponse);
+            }
+            Serial.print("Enviando respuesta");
+            Serial.println(jsonResponse);
+            webSocket.sendTXT(jsonResponse);
+
+}
